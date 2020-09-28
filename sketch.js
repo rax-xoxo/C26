2baa7fceca627bf972a859fe863dd5fa6af81344
Rashi260 <rashi.sharmafeb26@gmail.com>
2020-09-28 17:50:53 +0530

    sketch

--- sketch.js
+++ sketch.js
@@ -6,6 +6,8 @@ var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
 
+var box6;
+
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
 }
@@ -36,6 +38,8 @@ function setup(){
 
     bird = new Bird(100,100);
 
+    box6 = new Box(130,300,70,70);
+
 }
 
 function draw(){
@@ -61,4 +65,5 @@ function draw(){
 
     bird.display();
     platform.display();
+    box6.display();
 }
